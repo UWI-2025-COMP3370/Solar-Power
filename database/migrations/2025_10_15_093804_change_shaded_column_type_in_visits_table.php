0cb7404168cc8f159bbac2a5a98d058ba6de434b
Hakeem Best <235552321+Shotta254@users.noreply.github.com>
2025-10-15 05:52:32 -0400

    Migration

--- database/migrations/2025_10_15_093804_change_shaded_column_type_in_visits_table.php
+++ database/migrations/2025_10_15_093804_change_shaded_column_type_in_visits_table.php
@@ -1,8 +1,10 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class ChangeShadedColumnTypeInVisitsTable extends Migration
 {
     public function up(): void
     {
@@ -14,7 +16,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('visits', function (Blueprint $table) {
-            $table->boolean('shaded')->change(); // revert to integer/boolean
+            $table->boolean('shaded')->change(); // revert to boolean
         });
     }
-};
+}
