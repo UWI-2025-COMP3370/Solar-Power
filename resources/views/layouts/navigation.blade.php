154b47880dfd7feb29a6d5f94301ceee9404dedd
Hakeem Best <235552321+Shotta254@users.noreply.github.com>
2025-10-15 11:07:35 -0400

    Dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('customer')" :active="request()->routeIs('dashboard')">
+                        {{ __('Customer Records') }}
+                    </x-nav-link>
                 </div>
             </div>
 
