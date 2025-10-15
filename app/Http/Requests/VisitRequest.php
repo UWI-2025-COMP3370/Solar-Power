baf4feef7fc49258f77fb5b4af31293b2bd9a31e
Hakeem Best <235552321+Shotta254@users.noreply.github.com>
2025-10-15 05:35:20 -0400

    Request

--- app/Http/Requests/VisitRequest.php
+++ app/Http/Requests/VisitRequest.php
@@ -28,7 +28,7 @@ class VisitRequest extends FormRequest
             'roof_size' => 'required|numeric|min:0',
             'roof_type' => 'required|in:gable,hip,flat,other',
             'monthly_consumption_kwh' => 'required|numeric|min:0',
-            'shaded' => 'required|in:yes,no,partially',
+            'shaded' => 'required|in:Yes,No,Partially',
             'notes' => 'required|string|max:1000',
         ];
     }
