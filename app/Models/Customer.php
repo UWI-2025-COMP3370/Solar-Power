ea84314950ce472ad00f363fa5aec8be94adf21d
Hakeem Best <235552321+Shotta254@users.noreply.github.com>
2025-10-14 21:15:02 -0400

    Customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -1,26 +1,17 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+namespace App\Models;
 
-return new class extends Migration
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
 {
-    public function up(): void
-    {
-        Schema::create('customers', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('address');
-            $table->string('mobile');
-            $table->string('telephone')->nullable();
-            $table->string('email')->unique();
-            $table->timestamps();
-        });
-    }
-
-    public function down(): void
-    {
-        Schema::dropIfExists('customers');
-    }
-};
+    // allow mass assignment for these fields
+    protected $fillable = [
+        'name',
+        'address',
+        'mobile',
+        'telephone',
+        'email',
+    ];
+}
