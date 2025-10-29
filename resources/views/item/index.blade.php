168ca37b61c5d759f22321de8d3efba1ba8855b5
Hakeem Best <235552321+Shotta254@users.noreply.github.com>
2025-10-29 04:59:36 -0400

    404

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -1,53 +1,50 @@
 <x-app-layout>
     @auth
+        <!-- Create Item Button -->
         <form method="GET" action="{{ route('item.create') }}">
             @csrf
             <x-input-error :messages="$errors->get('message')" class="mt-2" />
-            <x-primary-button class="mt-4">{{ __('Create') }}
-            </x-primary-button>
+            <x-primary-button class="mt-4">{{ __('Create') }}</x-primary-button>
         </form>
+
+        <!-- Items List -->
         @foreach ($items as $item)
-            <div>{{ $item->name }}
+            <div class="my-4">
+                <span>{{ $item->name }}</span>
+
+                <!-- View Item -->
                 <form method="GET" action="{{ route('item.show', $item) }}" class="inline-block">
                     @csrf
-                    <x-input-error :messages="$errors->get('message')" class="mt-2" />
-                    <x-primary-button class="mt-4">{{ __('View') }}
-                    </x-primary-button>
+                    <x-primary-button class="mt-4">{{ __('View') }}</x-primary-button>
                 </form>
-                @if (Auth::user()->role == 'piDSSAdministrator')
+
+                <!-- Edit/Delete for Admin -->
+                @if(Auth::user()->role == 'piDSSAdministrator')
                     <form method="GET" action="{{ route('item.edit', $item) }}" class="inline-block">
                         @csrf
-                        <x-primary-button class="mt-4">
-                            {{ __('Edit') }}
-                        </x-primary-button>
+                        <x-primary-button class="mt-4">{{ __('Edit') }}</x-primary-button>
                     </form>
                     <form method="POST" action="{{ route('item.destroy', $item) }}" class="inline-block">
                         @csrf
                         @method('DELETE')
-                        <x-input-error :messages="$errors->get('message')" class="mt-2" />
-                        <x-primary-button class="mt-4">
-                            {{ __('Delete') }}
-                        </x-primary-button>
+                        <x-primary-button class="mt-4">{{ __('Delete') }}</x-primary-button>
                     </form>
                 @endif
-                @if (Auth::user()->role == 'Registered Customer')
-                    <form method="GET" action="{{"wishList/addItemToList/".$item->id."/1/"}}"
-                        class="inline-block">
-                        @csrf
-                        @method('GET')
-                        <x-input-error :messages="$errors->get('message')" class="mt-2" />
-                        <x-primary-button class="mt-4">
-                            {{ __('Add to Wish List') }}
-                        </x-primary-button>
-                    </form>
-            </div>
-        @endif
-        @endforeach
-    @endauth
 
-    <!-- Back Button -->
-    <x-primary-button class="mt-4 bg-gray-500 hover:bg-gray-600">
-        <a href="{{ url()->previous() }}">{{ __('Back') }}</a>
-    </x-primary-button>
-    
+                <!-- Add to Wishlist for Customer -->
+                @if(Auth::user()->role == 'Registered Customer')
+                    <form method="POST" action="{{ route('wishlist.add', $item->id) }}" class="inline-block">
+                        @csrf
+                        <input type="hidden" name="quantity" value="1">
+                        <x-primary-button class="mt-4">{{ __('Add to Wish List') }}</x-primary-button>
+                    </form>
+                @endif
+            </div>
+        @endforeach
+
+        <!-- Back Button -->
+        <x-primary-button class="mt-4 bg-gray-500 hover:bg-gray-600">
+            <a href="{{ url()->previous() }}">{{ __('Back') }}</a>
+        </x-primary-button>
+    @endauth
 </x-app-layout>
