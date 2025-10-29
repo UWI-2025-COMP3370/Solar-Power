aea86ea2be1b3940d84a13860dfa3220aa3bda13
Hakeem Best <235552321+Shotta254@users.noreply.github.com>
2025-10-29 04:39:41 -0400

    Show

--- resources/views/wishlist/show.blade.php
+++ resources/views/wishlist/show.blade.php
@@ -7,35 +7,36 @@
         @endphp
 
         <table class="dark:text-white w-full border-collapse">
-            <tr>
-                <th>Product</th>
-                <th>Price</th>
-            </tr>
-
-            @csrf
-
-            @if($wishList && $wishList->items->count())
-                @foreach($wishList->items as $item)
+            <thead>
+                <tr>
+                    <th class="border-b border-gray-300 dark:border-gray-700 p-2 text-left">Product</th>
+                    <th class="border-b border-gray-300 dark:border-gray-700 p-2 text-right">Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse(optional($wishList)->items ?? [] as $item)
                     <tr>
-                        <td>{{ $item->name }}</td>
-                        <td>${{ number_format($item->price, 2) }}</td>
+                        <td class="border-b border-gray-300 dark:border-gray-700 p-2">{{ $item->name }}</td>
+                        <td class="border-b border-gray-300 dark:border-gray-700 p-2 text-right">${{ number_format($item->price, 2) }}</td>
                         @php $total += $item->price; @endphp
                     </tr>
-                @endforeach
-
-                <tr>
-                    <td><strong>Total:</strong></td>
-                    <td><strong>${{ number_format($total, 2) }}</strong></td>
-                </tr>
-            @else
-                <tr>
-                    <td colspan="2">Your wishlist is empty.</td>
-                </tr>
-            @endif
+                @empty
+                    <tr>
+                        <td colspan="2" class="p-2">Your wishlist is empty.</td>
+                    </tr>
+                @endforelse
+                @if(optional($wishList)->items->count())
+                    <tr>
+                        <td class="p-2 font-bold">Total:</td>
+                        <td class="p-2 font-bold text-right">${{ number_format($total, 2) }}</td>
+                    </tr>
+                @endif
+            </tbody>
         </table>
+
+        <!-- Back Button -->
+        <a href="{{ url()->previous() }}" class="mt-4 inline-block bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded">
+            {{ __('Back') }}
+        </a>
     @endauth
-    <!-- Back Button -->
-    <x-primary-button class="mt-4 bg-gray-500 hover:bg-gray-600">
-        <a href="{{ url()->previous() }}">{{ __('Back') }}</a>
-    </x-primary-button>
 </x-app-layout>
