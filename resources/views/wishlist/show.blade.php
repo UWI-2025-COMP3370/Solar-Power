638f2921af71118b8f5fe5d5ba7ae86b631b5606
Hakeem Best <235552321+Shotta254@users.noreply.github.com>
2025-10-29 04:50:27 -0400

    Wish

--- resources/views/wishlist/show.blade.php
+++ resources/views/wishlist/show.blade.php
@@ -19,13 +19,13 @@
                         <tr class="border-b border-gray-300 dark:border-gray-700">
                             <td class="p-2">{{ $item->name }}</td>
                             <td class="p-2 text-center">
-                                <input type="number" min="1" 
+                                <input type="number" min="1"
                                     x-model.number="quantities[{{ $item->id }}]"
                                     x-on:input.debounce.300ms="updateItem({{ $item->id }}, quantities[{{ $item->id }}])"
                                     class="w-16 text-center border rounded px-1 py-0.5">
                             </td>
                             <td class="p-2 text-right">
-                                $<span x-text="formatPrice(itemTotals[{{ $item->id }}] ?? {{ $item->price }})"></span>
+                                $<span x-text="formatPrice(itemTotals[{{ $item->id }}] ?? {{ $item->price * ($item->pivot->quantity ?? 1) }})"></span>
                             </td>
                         </tr>
                     @empty
