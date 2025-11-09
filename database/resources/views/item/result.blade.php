ea9ff7b0d6540adcd719905161ad32ed30ccfd85
Shotta254 <235552321+Shotta254@users.noreply.github.com>
2025-11-09 09:01:53 -0400

    Delete database directory

--- database/resources/views/item/result.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
- @auth
-Results:
- @foreach($items as $item)
- <div>{{ $item->name }}{{ $item->price }}{{ $item->description }}
-</div>
- @endforeach
- @endauth
-</x-app-layout>
--- database/resources/views/visit/show.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    @auth
-        <h3>piDSS Visit</h3>
-        <div>
-            {{ $visit->technician_name }}, {{ $visit->customer_name }}, {{ $visit->customer_email }}, {{ $visit->roof_size }},
-            {{ $visit->roof_type }}, {{ $visit->monthly_consumption_kwh }}, {{ $visit->shaded }}, {{ $visit->notes }},
-        </div>
-@endauth
-</x-app-layout>
